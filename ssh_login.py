c8d41004132ce088859a3988ac06363b01416f45
Valentin <admin@hessfr.fr>
2025-09-17 11:09:10 +0200

    Missing IP for SQL Server

--- ssh_login.py
+++ ssh_login.py
@@ -4,7 +4,7 @@ from fabric import Connection
 def choix_serveur():
     choix=0
     resp=["1","2","3"]
-    ip=["192.168.157.136","192.168.157.138",""]
+    ip=["192.168.157.136","192.168.157.138","192.168.157.140"]
     while choix not in resp :
         choix=str(input("Quel est le serveur de destination (1-> FTP, 2-> Web, 3-> SQL)"))
     return ip[choix-1]
