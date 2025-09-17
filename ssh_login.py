96f26296398b4fdad608dd63f06e84d2b4aa2b08
Valentin <admin@hessfr.fr>
2025-09-17 11:22:33 +0200

    Add connection

--- ssh_login.py
+++ ssh_login.py
@@ -1,6 +1,7 @@
 #pip install fabric ou le paquet python-fabric (dans les dépots AUR sur Arch) / python3-fabric (Debian)
 from fabric import Connection
 
+SSH_key=''
 def choix_serveur():
     choix=0
     resp=["1","2","3"]
@@ -8,3 +9,13 @@ def choix_serveur():
     while choix not in resp :
         choix=str(input("Quel est le serveur de destination (1-> FTP, 2-> Web, 3-> SQL)"))
     return ip[choix-1]
+
+def connection_serveur(key,ip):
+    c = Connection(
+        host=ip
+        user="monitor"
+        connect_kwargs={
+            "key_filename": key
+        }
+    )
+    return c
