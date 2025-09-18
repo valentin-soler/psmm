accbf51f60c7d21919967ffd82cfb79f0afba4ae
Valentin <admin@hessfr.fr>
2025-09-18 10:46:10 +0200

    Add comment

--- ssh_mysql.py
+++ ssh_mysql.py
@@ -1,20 +1,21 @@
+#Voir README pour l'installation de la dépendance
 import mysql.connector
 
-
+#Création de la connection
 conn = mysql.connector.connect(
     host="192.168.157.140",
     user="bilou",
     password="", #A changé avec votre mdp
     database="test"
 )
-
+#Connection
 cursor = conn.cursor()
 cursor.execute("SHOW TABLES")
 tables = cursor.fetchall()
-
+#On affiches les tables
 print("Tables dans la base MariaDB :")
 for table in tables:
     print(table[0])
-
+#On ferme la connection
 cursor.close()
 conn.close()
