a6ec77cf731b9ef474a67eb1d12766f953a9dcba
Valentin <admin@hessfr.fr>
2025-09-19 09:57:10 +0200

    add ssh_ftp_error.py

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ ssh_login.py -> Connection en SSH et execution d'une commande
 ssh_login_sudo.py -> Connection en SSH et execution d'une commande en sudo (sudo configurer sans demande de mot de passe)
 ssh_mysql.py --> Connection au serveur SQL et liste les tables dans une base de donnée définie dans les script avec les id donnée dans le script
 ssh_mysql_error.py --> Connection au serveur SQL et récupération des logs pour les mettres dans une BDD
+ssh_ftp_error.py --> Connection au serveur FTP via SSH pour récupération des log, et stockages des tentative de connection infructueuse dans la bdd
--- ssh_ftp_error.py
+++ ssh_ftp_error.py
@@ -4,7 +4,7 @@ import mysql.connector
 import re
 
 SSH_key='PSMM' #Nom de la clé privé pour se connecté, cela peut être un chemin aussi.
-ip='192.168.157.140' #IP du serveur SQL
+ip='192.168.157.136' #IP du serveur SQL
 
 def connection_serveur_ssh(key,ip):
     c = Connection(
@@ -33,7 +33,7 @@ def connection_close_sql(connection):
 def add_to_DB(user,ip,date,time,connection):
     cursor = connection.cursor()
     cursor.execute(
-    "INSERT INTO error_sql (user, date, time, ip) VALUES (%s, %s, %s, %s)",
+    "INSERT INTO error_ftp (user, date, time, ip) VALUES (%s, %s, %s, %s)",
     (user, date, time, ip))
     cursor.close()
 
@@ -42,7 +42,7 @@ def exec_command(srv,command):
 
 def extract_log(logs):
     result=[]
-    pattern = re.compile(r'^(?P<date>\d{4}-\d{2}-\d{2})\s+(?P<heure>\d{2}:\d{2}:\d{2}).*Access denied for user \'(?P<user>[^\']+)\'@\'(?P<ip>[^\']+)\'')
+    pattern = re.compile(r'^(?P<date>\w{3} \w{3} \d{1,2}) (?P<heure>\d{2}:\d{2}:\d{2}) (?P<annee>\d{4}) \[pid \d+\] \[(?P<user>.*?)\].*Client "(?P<ip>[\d\.]+)", "530 Login incorrect\."')
     logs_lines=logs.splitlines()
     for log in logs_lines:
         m = pattern.search(log)
@@ -55,9 +55,8 @@ def extract_log(logs):
             })
     return result
 
-
 def main():
-    command_log=f"sudo cat /var/log/mysql/error.log"
+    command_log=f"sudo cat /var/log/vsftpd/vsftpd.log"
     srv_ssh=connection_serveur_ssh(SSH_key,ip)
     logfile=exec_command(srv_ssh,command_log)
     result=extract_log(logfile)
@@ -66,17 +65,6 @@ def main():
         print(r["user"],r["ip"],r["date"],r["heure"])
         add_to_DB(r["user"],r["ip"],r["date"],r["heure"],srv_sql)
     connection_close_sql(srv_sql)
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
