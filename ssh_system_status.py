831a60c63f3686fc0c33aa7d6334a72ceb262c3d
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2025-10-03 14:42:15 +0200

    aled

--- ssh_system_status.py
+++ ssh_system_status.py
@@ -3,7 +3,6 @@ import mysql.connector
 from datetime import datetime
 
 SSH_key='PSMM' #Nom de la clé privé pour se connecté, cela peut être un chemin aussi.
-ip='192.168.157.140' #IP du serveur SQL
 
 def connection_serveur_ssh(key,ip):
     c = Connection(
@@ -17,7 +16,8 @@ def connection_serveur_ssh(key,ip):
 
 def connection_serveur_sql():
     c = mysql.connector.connect(
-        host="192.168.157.140",
+        host="192.168.157.128", #IP du serveur SQL PC Fixe
+#         host="192.168.157.140", #IP du serveur SQL PC Portable
         user="",
         password="",
         database="error_access"
@@ -44,15 +44,30 @@ def collect_stats(c):
     disk_total = disk_line[1]
 
     return used_ram, total_ram, cpu_usage, disk_used, disk_total
+
 def add_to_DB(machine,date,heure,cpu,ram,disk,c):
     cursor = c.cursor()
     cursor.execute("INSERT IGNORE INTO etat_sys (machine, date, heure, cpu, ram, disk) VALUES (%s, %s, %s, %s, %s, %s)",
     (machine,date,heure,cpu,ram,disk))
     cursor.close()
+
+def script_machine(ip,type_vm):
+    server_ssh=connection_serveur_ssh(SSH_key,ip)
+    result=collect_stats(server_ssh)
+    server_sql=connection_serveur_sql()
+    result_ram=f"{result[0]}M of {result[1]}M"
+    result_cpu_usage=result[2]
+    result_disk=f"{result[3]} of {result[4]}"
+    add_to_DB(type_vm,datetime.now.date(),datetime.now.time(),result_cpu_usage,result_ram,result_disk)
+    connection_close_sql(server_sql)
     
 def main():
-    server_ssh=connection_serveur_ssh(SSH_key,ip)
-    print(collect_stats(server_ssh))
+    #Server SQL
+    script_machine("192.168.157.128","SQL")
+    #Server FTP
+    script_machine("192.168.157.130","FTP")
+    #Server WEB
+    script_machine("192.168.157.129","WEB")
 
 if __name__ == "__main__":
     main()
